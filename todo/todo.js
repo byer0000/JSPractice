a2fa1108ea0219c8167e41e0efd37e03d8ce53c2
Yuta <>
2024-05-23 17:10:21 +0900

    不要コメント

--- todo/todo.js
+++ todo/todo.js
@@ -1,22 +1,2 @@
 let input = prompt('入力してください')
-
-// const todos = ['水やり','掃除']
-
-// while(input != 'quit') {
-
-//     if(input == 'list') {
-//         for (let i = 0; i < todos.length; i++) {
-//             console.log(`${i}: ${todos[i]}`);
-//         }
-//         console.log('****************************');
-//     }else if(input == 'new') {
-//         const newTodo = prompt('新しい値を追加');
-//         todos.push(newTodo);
-//         console.log(`${newTodo}を追加`)
-//     }
-
-//     input = prompt('違う値を入力')
-// }
-
-// console.log('finish')
-
+const list = ['起きる','寝る'];

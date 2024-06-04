2a8c2daa596dc41e038b75a573abc5f8228215b0
Yuta <>
2024-06-04 18:16:31 +0900

    高階関数

---
+++ hiOrderFunc/app.js
@@ -0,0 +1,41 @@
+function callTwice(func) {
+    func();
+    func();
+}
+
+let rollDie = function () {
+    const roll = Math.floor(Math.random() * 6) + 1;
+    console.log(roll)
+}
+
+callTwice(rollDie);
+
+// 関数から関数を返す
+function makeRandomFunc() {
+    const rand = Math.random();
+    if (rand > 0.5) {
+        return function () {
+            console.log('おめでとう');
+        }
+    } else {
+        return function() {
+            alert('ウィルス感染');
+            alert('閉じないで');
+        }
+    }
+}
+
+function BetweenFunc(min, max) {
+    return function(num){
+        return num >= min && num <= max;
+    }
+}
+
+function isBetween(num) {
+    return num >= 50 && num <= 100;
+}
+
+function isBetween(num) {
+    return num >= 1 && num <= 10;
+}
+

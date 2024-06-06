945c557c3a98d8d662c02e4ba4014dafab64a5e5
Yuta <>
2024-06-06 18:20:29 +0900

    ArrowFunctions

---
+++ ArrowFunctions/app.js
@@ -0,0 +1,27 @@
+// const add = function (x, y) {
+//     return x + y;
+// }
+
+// const add = (x, y) => {
+//     return x + y;
+// }
+
+const square = (num) => {
+    return num * num;
+}
+
+// const random = () => {
+//     return Math.random();
+// }
+
+const random = () => (
+    Math.random()
+);
+
+// addの省略
+// const add = (x, y) => (
+//     x + y
+// );
+
+// 一行の場合はさらに省略
+const add = (x, y) => x + y;
